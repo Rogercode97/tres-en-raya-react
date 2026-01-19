c5c29e6a80d3dc14cbb39f31aab7df53c274bd9d
Roger <rogercarp97@gmail.com>
2026-01-19 12:53:15 -0300

    ✨ Proyecto completado: Juego funcional con diseño mejorado

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,7 +1,16 @@
-export default function Square({ value, onSquareClick }) {
+function Square({ value, onSquareClick }) {
+  // Esta función decide qué clase CSS usar
+  // Si 'value' es "X" o "O", añade esa clase además de "square"
+  const squareClass = `square ${value ? value.toLowerCase() : ''}`;
+  
   return (
-    <button className="square" onClick={onSquareClick}>
+    <button 
+      className={squareClass}  // Esto creará "square x" o "square o"
+      onClick={onSquareClick}
+    >
       {value}
     </button>
   );
 }
+
+export default Square;
